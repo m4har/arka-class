47de00302ca216e9bbed72fd19556a4047c040d7
mahardika <mahardicka404@gmail.com>
2019-11-10 15:11:01 +0700

    reusable component

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,9 @@
 import React,{Component} from 'react'
 import {InputGroup, FormControl, Container, Button, Table, Modal} from 'react-bootstrap'
 import Axios from 'axios'
+import HeaderInput from './components/HeaderInput'
+import DataTable from './components/Table'
+import DetailModal from './components/DetailModal'
 
 const url = 'http://127.0.0.1:3001'
 
@@ -105,99 +108,30 @@ class App extends Component{
     return (
       <Container>
         {/* Tampilan From */}
-        <InputGroup style={{margin:10}} className="mb-3">
-         <InputGroup.Prepend>
-            <InputGroup.Text id="basic-addon1">A</InputGroup.Text>
-          </InputGroup.Prepend>
-          <FormControl
-            onChange={(event)=>this.setState({nama:event.target.value})}
-            style={{marginRight:10}}
-            placeholder="Nama"
-            aria-label="Nama"
-            aria-describedby="basic-addon1"
-          />
-          <InputGroup.Prepend>
-            <InputGroup.Text id="basic-addon1">B</InputGroup.Text>
-          </InputGroup.Prepend>
-          <FormControl
-            onChange={(event)=>this.setState({umur:event.target.value})}
-            style={{marginRight:10}}
-            placeholder="Umur"
-            aria-label="Umur"
-            aria-describedby="basic-addon1"
-          />
-          <InputGroup.Prepend>
-            <InputGroup.Text id="basic-addon1">C</InputGroup.Text>
-          </InputGroup.Prepend>
-          <FormControl
-          onChange={(event)=>this.setState({alamat:event.target.value})}
-           style={{marginRight:10}}
-            placeholder="Alamat"
-            aria-label="Alamat"
-            aria-describedby="basic-addon1"
-          />
-          <Button onClick={()=>this.postDataFromApi()} variant='primary'>ADD</Button>
-         </InputGroup>
+        <HeaderInput
+          onChangeName={(event)=>this.setState({nama:event.target.value})}
+          onChangeAge={(event)=>this.setState({umur:event.target.value})}
+          onChangeAddr={(event)=>this.setState({alamat:event.target.value})}
+          onClick={()=>this.postDataFromApi()}
+        />
         {/* Tampilan Table */}
-        <Table striped bordered hover variant="dark">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Nama</th>
-              <th>Umur</th>
-              <th>Alamat</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-           {this.state.data.map((item, index)=>(
-             <tr key={index}>
-              <td>{index+1}</td>
-              <td>{item.nama}</td>
-              <td>{item.umur}</td>
-              <td>{item.alamat}</td>
-              <td style={{width:50}}>
-                <Button variant='warning' onClick={()=>this.showModal(item)}>Edit/Delete</Button>
-              </td>
-           </tr>
-           ))}
-          </tbody>
-        </Table>
+        <DataTable
+          data={this.state.data}
+          onClick={(item)=>this.showModal(item)}
+        />
         {/* Modal */}
-
-        <Modal show={this.state.showModal} onHide={()=>this.hideModal()}>
-        <Modal.Header closeButton>
-          <Modal.Title>Edit Data</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <FormControl 
-          value={this.state.dNama} 
-          onChange={(event)=>this.setState({dNama:event.target.value})} 
-          style={{marginBottom:10}} 
-          placeholder='Nama'
-          />
-          <FormControl 
-          value={this.state.dUmur} 
-          onChange={(event)=>this.setState({dUmur:event.target.value})} 
-          style={{marginBottom:10}} 
-          placeholder='Umur'
-          />
-          <FormControl 
-          value={this.state.dAlamat} 
-          onChange={(event)=>this.setState({dAlamat:event.target.value})} 
-          style={{marginBottom:10}} 
-          placeholder='Alamat'
-          />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="danger" onClick={()=>this.deleteDataApi()}>
-            Delete
-          </Button>
-          <Button variant="primary" onClick={()=>this.editDataApi()}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
+        <DetailModal
+          show={this.state.showModal}
+          onHide={()=>this.hideModal()}
+          valueName={this.state.dNama}
+          valueAge={this.state.dUmur}
+          valueAddr={this.state.dAlamat}
+          onChangeAge={(event)=>this.setState({dUmur:event.target.value})}
+          onChangeName={(event)=>this.setState({dNama:event.target.value})}
+          onChangeAddr={(event)=>this.setState({dAlamat:event.target.value})}
+          onDelete={()=>this.deleteDataApi()}
+          onSave={()=>this.editDataApi()}
+        />
       </Container>
     )
   }
